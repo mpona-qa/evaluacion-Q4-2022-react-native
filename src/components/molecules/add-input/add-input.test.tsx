2712dddea7c1e3ed32632ad590efc24fab482efa
mpona <mpona@pichincha.com>
2023-04-09 19:34:43 -0500

    add test

---
+++ src/components/molecules/add-input/add-input.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {fireEvent, render, screen} from '@testing-library/react-native';
+import {AddInput} from './add-input';
+import AddIcon from '../../../utils/assets/AddIcon.png';
+
+describe('AddInput Component', () => {
+  it('Should render AddInput components', () => {
+    render(<AddInput onAdd={jest.fn} placeholder="Add Gif" image={AddIcon} />);
+
+    const input = screen.getByPlaceholderText('Add Gif');
+    expect(input).toBeDefined();
+    const icon = screen.getByTestId('iconID');
+    expect(icon).toBeDefined();
+  });
+
+  it('Should execute onHandleButtonPress and execute onAdd', () => {
+    const mockOnChange = jest.fn();
+    render(
+      <AddInput onAdd={mockOnChange} placeholder="Add Gif" image={AddIcon} />,
+    );
+    const input = screen.getByPlaceholderText('Add Gif');
+    fireEvent.changeText(input, 'nuevo gif');
+    const icon = screen.getByTestId('iconID');
+    fireEvent.press(icon);
+    expect(mockOnChange).toBeCalledWith('nuevo gif');
+  });
+});
