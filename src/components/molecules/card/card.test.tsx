ef810dab18d7566d42c7c7a189d996ab9f95670c
mpona <mpona@pichincha.com>
2023-04-09 20:48:17 -0500

    add test card

---
+++ src/components/molecules/card/card.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {fireEvent, render, screen} from '@testing-library/react-native';
+import {Card} from './card';
+
+describe('Card Component', () => {
+  const gifExample = [
+    {
+      id: 2,
+      url: 'https://media.tenor.com/Q4qyZizrNGMAAAAi/thor-love-and-thunder-marvel-studios.gif',
+    },
+  ];
+  it('Should render Card component', () => {
+    render(<Card gif={gifExample} onDeleteGif={jest.fn()} />);
+  });
+  it('Should render Card component with gif=[]', () => {
+    render(<Card gif={[]} onDeleteGif={jest.fn()} />);
+  });
+
+  it('Should execute onDeleteGif when the remove button is clicked', () => {
+    const mockOnDeleteGif = jest.fn();
+    render(<Card gif={gifExample} onDeleteGif={mockOnDeleteGif} />);
+    const button = screen.getByTestId('iconID');
+    fireEvent.press(button);
+    expect(mockOnDeleteGif).toBeCalledWith(gifExample[0]);
+  });
+});
